6f89aa8fe178e46b8b01cf1080466f9ea0aad5d4
rushemaa <rushemaa@gmail.com>
2023-05-15 15:49:36 +0200

    Restructure project

---
+++ src/components/Leftnav.js
@@ -0,0 +1,10 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import '../css/Left-nav.css';
+
+export default function LeftNav() {
+  return (
+    <div className="left-side">
+      <AccountCircleIcon />
+    </div>
+  );
+}
